5c553c037a95929d3f9b23831b8af97ad8c5d5c4
Govind <govindsys1008@gmail.com>
2025-12-03 20:00:49 +0530

    added internal report upload endpoint

--- djangoproj/djangoapp/schema.py
+++ djangoproj/djangoapp/schema.py
@@ -1,1 +1,13 @@
 from ninja import Schema
+from typing import Optional, List
+class InputSchema(Schema):
+    text: str
+    molecule : Optional[str]  = None
+    tasks : Optional[List[str]] = None
+    region : Optional[str] = None
+    timeframe : Optional[int] = 5
+
+
+class GetSignedUrl(Schema):
+    file_name: str
+    content_type : str
