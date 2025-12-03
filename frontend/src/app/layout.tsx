a184702e9233c538a8faebb81d35502e217cffb2
Govind S <govindsys1008@gmail.com>
2025-12-03 11:44:41 +0000

    added dashboard, home pages and cognito config pages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConfigureAmplifyClientSide from "@/app/amplify-cognito-config";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConfigureAmplifyClientSide />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
       </body>
     </html>
   );
