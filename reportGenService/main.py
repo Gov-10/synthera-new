6cda74e37fcfc7850db8a28741430b0a1ff96a2c
Govind <govindsys1008@gmail.com>
2026-06-19 14:13:11 +0530

    revamp nearly completed

--- reportGenService/main.py
+++ reportGenService/main.py
@@ -1,48 +1,30 @@
-from fastapi import FastAPI, HTTPException, Response, Depends
-from sqlalchemy.orm import Session
-import os, logging, json, boto3
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
-from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (OTLPSpanExporter)
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry import trace
+from kafka import KafkaConsumer
+import os, json
 from dotenv import load_dotenv
 load_dotenv()
-app = FastAPI()
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-logger = logging.getLogger("report")
-resource = Resource.create({"service.name": "report-service"})
-provider = TracerProvider(resource=resource)
-trace.set_tracer_provider(provider)
-otlp_exporter = OTLPSpanExporter( endpoint="jaeger.tracing.svc.cluster.local:4317",insecure=True,)
-provider.add_span_processor(
-    BatchSpanProcessor(otlp_exporter)
-)
-FastAPIInstrumentor.instrument_app(app)
-tracer = trace.get_tracer(__name__)
-from datebase import Reports, sessionLocal
-def get_db():
-    db=sessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from redis import Redis
+redis_client=Redis(host=os.getenv("REDIS_HOST"), port=int(os.getenv("REDIS_PORT")), password=os.getenv("REDIS_PASSWORD"), decode_responses=True)
+from langchain_groq import ChatGroq
+llm = ChatGroq(model="", api_key=os.getenv("GROQ_API_KEY"))
+BOOTSTRAP_SERVER=""
+consumer=KafkaConsumer("iqvia-worker", "patent-worker", "web-worker", "exim-worker", bootstrap_servers=BOOTSTRAP_SERVER, value_deserializer=lambda x: json.loads(x.decode()), group_id="report-group")
+store = {}
+for msg in consumer:
+    event = msg.value
+    topic = msg.topic
+    job_id=event["job_id"]
+    if job_id not in store:
+        store[job_id] = {}
+    if topic == "iqvia-worker":
+        store[job_id]["iqvia"] = data["iqvia_res"]
+    if topic == "patent-worker":
+        store[job_id]["patent"] = data["patent_res"]
+    if topic == "web-worker":
+        store[job_id]["web"] = data["web_res"]
+    if topic=="exim-worker":
+        store[job_id]["exim"] = data["exim_res"]
+    report= store[job_id]
+    if ("iqvia" in report and "patent" in report and "web" in report and "exim" in report):
+        pass
 
-@app.on_event("startup")
-def startup():
-    logger.info(json.dumps({"event": "report_service_start", "pod": os.getenv("HOSTNAME")}))
-
-@app.get("/")
-def chek():
-    return {"status": "Running"}
-
-@app.get("/metrics")
-def metrics():
-    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
-
-
-
-
+    
